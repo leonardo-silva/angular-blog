0f2dc39baafad851e843a217dfc00c077eff732b
Leonardo Silva <leonardosilvaif@hotmail.com>
2023-10-13 13:40:39 -0300

    feat: using dataFake array to fill card components

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,13 +1,23 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { dataFake } from 'src/app/data/dataFake';
 
 @Component({
   selector: 'app-big-card',
   templateUrl: './big-card.component.html',
   styleUrls: ['./big-card.component.css']
 })
-export class BigCardComponent {
-  @Input() photoCover: string = ""
-  @Input() cardTitle: string = "Lorem haha ipsum dolor sit amet, consectetur adipiscing elit."
-  @Input() cardDescription: string = ""
-  @Input() id: string = "0"
+export class BigCardComponent implements OnInit {
+  photoCover: string = ""
+  cardTitle: string = ""
+  cardDescription: string = ""
+  @Input() id: string = ""
+
+  ngOnInit(): void {
+    const data = dataFake.filter((item) => item.id === this.id)[0];
+    if (data) {
+      this.cardDescription = data.description;
+      this.cardTitle = data.title;
+      this.photoCover = data.photo;
+    }
+  }
 }
--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,12 +1,22 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+
+import { dataFake } from 'src/app/data/dataFake';
 
 @Component({
   selector: 'app-small-card',
   templateUrl: './small-card.component.html',
   styleUrls: ['./small-card.component.css']
 })
-export class SmallCardComponent {
-  @Input() photo: string = ""
-  @Input() title: string = ""
+export class SmallCardComponent implements OnInit {
+  photo: string = ""
+  title: string = ""
   @Input() id: string = "0"
+
+  ngOnInit(): void {
+    const data = dataFake.filter((item) => item.id === this.id)[0];
+    if (data) {
+      this.photo = data.photo;
+      this.title = data.title;
+    }
+  }
 }
